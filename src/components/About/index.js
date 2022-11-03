19f7eeaa01062a54a71f4882a6dae31c0fec5b40
thomas <thomasmccalister@gmail.com>
2022-11-03 10:42:29 -0500

    added about component

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './about.css'
+
+function About(){
+    <div id='aboutCont'>
+    <h1>My name is</h1>
+    <h5>Thomas McCalister</h5>
+    // update this later
+    <p>This is going to be a long paragraph derived from my cover letter including my updated experience. I will return to this</p>
+    </div>
+}
+
+export default About;
---
+++ src/components/Landing/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './landing.css'
+
+function Landing(){
+<div id='landingCont'>
+    <h1>Welcome!</h1>
+</div>
+}
+
+export default Landing;
