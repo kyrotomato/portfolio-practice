17268d951a9908c455365d9735ce0004e4b2e0b6
thomas <thomasmccalister@gmail.com>
2022-11-03 10:19:41 -0500

    intial commit and first component test suceeded

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './header.css';
+
+function Header(){
+    return(
+        <div id='headerCont'>
+            <nav id='nav'>
+                <a id='home'>Home</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
